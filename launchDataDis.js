1c4d698f4ff4a6c6b3605b8341b1ce00a9ebc77e
Aimie McElroy <113055542+Aimie-McElroy@users.noreply.github.com>
2022-11-14 11:17:28 -0700

    Add files via upload

---
+++ launchDataDis.js
@@ -0,0 +1,65 @@
+//a class for setting up the data from dataloader
+class InputData{
+    constructor(
+        time_seconds,
+        latitude,
+        longitude,
+        gps_altitude,
+        bmpSensor_altitude,
+        bmpSensor_pressure,
+        bmpSensor_temp,
+        digSensor_temp,
+        cssSensor_temp,
+        cssSensor_eCO2,
+        cssSensor_TVOC,
+        UV,
+        accelX,
+        accelY,
+        accelZ,
+        magneticX,
+        magneticY,
+        magneticZ,
+        gyroX,
+        gyroY,
+        gyroZ,
+    ){
+        this.time_seconds = time_seconds;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.gps_altitude = gps_altitude;
+        this.bmpSensor_altitude = bmpSensor_altitude;
+        this.bmpSensor_pressure = bmpSensor_pressure;
+        this.bmpSensor_temp =  bmpSensor_temp;
+        this.digSensor_temp = digSensor_temp;
+        this.cssSensor_temp = cssSensor_temp;
+        this.cssSensor_eCO2 = cssSensor_eCO2;
+        this.cssSensor_TVOC = cssSensor_TVOC;
+        this.UV = UV;
+        this.accelX = accelX;
+        this.accelY = accelY;
+        this.accelZ = accelZ;
+        this.magneticX = magneticX;
+        this.magneticY = magneticY;
+        this.magneticZ = magneticZ;
+        this.gyroX = gyroX;
+        this.gyroY = gyroY;
+        this.gyroZ = gyroZ;
+    }
+}
+
+//the getData function extracts the data from the dataloader.js file
+function getData(){ 
+var loadedData = loadData();
+return loadedData;
+}
+
+//function to setup data in the table 
+function dataRow(legend, value, units){
+    msg ="<td>";
+    msg += legend;
+    msg += ":</td><td";
+    msg += value;
+    msg += " " + units;
+    msg += "</td>";
+    return msg;
+}
